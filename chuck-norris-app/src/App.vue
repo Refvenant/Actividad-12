399214b6fc2b2699dc789a98adc6a49fda26b71c
Fernando De Leon Alvarado <fer@192.168.1.19>
2024-04-19 20:26:02 -0600

    Cambios de la tarea 10

--- chuck-norris-app/src/App.vue
+++ chuck-norris-app/src/App.vue
@@ -1,25 +1,47 @@
 <template>
-   <div id="app">
-    <h1>Chuck Norris Facts</h1>
-    <ul>
-      <li v-for="(fact, index) in chuckFacts" :key="index" class="fact">
-        {{ fact.value }}
-      </li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <chuck-card
+        v-for="(fact, index) in chuckFacts"
+        :key="index"
+        :iconUrl="fact.icon_url"
+        :value="fact.value"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import ChuckCard from './components/ChuckCard.vue';
+
 export default {
   name: 'App',
+  components: {
+    ChuckCard
+  },
   data() {
     return {
       chuckFacts: [
-        { value: "Chuck Norris can skydive into outer space." },
-        { value: "The chief export of Chuck Norris is pain." },
-        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
-        { value: "Time waits for no man. Unless that man is Chuck Norris." },
-        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
+        { 
+          "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+          "value": "Chuck Norris can skydive into outer space." 
+        },
+        { 
+          "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+          "value": "The chief export of Chuck Norris is pain." 
+        },
+        { 
+          "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+          "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
+        },
+        { 
+          "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+          "value": "Time waits for no man. Unless that man is Chuck Norris." 
+        },
+        { 
+          "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+          "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
+        }
       ]
     };
   }
@@ -27,16 +49,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Arial, sans-serif;
-  text-align: center;
-  margin-top: 50px;
-}
-
-.fact {
-  margin-bottom: 20px;
-  padding: 10px;
-  background-color: #f0f0f0;
-  border-radius: 5px;
-}
+/* Aquí puedes agregar estilos adicionales si es necesario */
 </style>
---
+++ chuck-norris-app/src/components/ChuckCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+      <div class="card">
+        <img :src="iconUrl" class="card-img-top" alt="Chuck Norris">
+        <div class="card-body">
+          <p class="card-text">{{ value }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ChuckCard',
+    props: {
+      iconUrl: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: String,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Aquí puedes agregar estilos adicionales si es necesario */
+  </style>
+  
