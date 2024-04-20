c3c18d3587863d5e286145ec17c294bd3a5c9eb8
Fernando De Leon Alvarado <fer@192.168.1.19>
2024-04-19 19:57:42 -0600

    Agrega archivos para la Actividad 12

---
+++ chuck-norris-app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+   <div id="app">
+    <h1>Chuck Norris Facts</h1>
+    <ul>
+      <li v-for="(fact, index) in chuckFacts" :key="index" class="fact">
+        {{ fact.value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      chuckFacts: [
+        { value: "Chuck Norris can skydive into outer space." },
+        { value: "The chief export of Chuck Norris is pain." },
+        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+        { value: "Time waits for no man. Unless that man is Chuck Norris." },
+        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+
+.fact {
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+}
+</style>
